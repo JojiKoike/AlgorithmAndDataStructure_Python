f12bc9dcc4d718fb170b476d386da49d8874d0ec
JojiKoike <joji.koike@gmail.com>
2019-01-15 17:59:17 +0900

    Recursive Sample (Koch Curve)

---
+++ chapter6/koch_curve.py
@@ -0,0 +1,33 @@
+from math import pi, cos, sin
+
+th = pi * 60.0 / 180.0
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+
+def koch(d: int, p1: Point, p2: Point):
+    if d == 0:
+        return
+    s = Point((p1.x * 2.0 + p2.x * 1.0) / 3.0, (p1.y * 2.0 + p2.y * 1.0) / 3.0)
+    t = Point((p1.x * 1.0 + p2.x + 2.0) / 3.0, (p1.y * 1.0 + p2.y * 2.0) / 3.0)
+    ux = (cos(th) - sin(th)) * (t.x - s.x) + s.x
+    uy = (sin(th) + cos(th)) * (t.y - s.y) + s.y
+    u = Point(ux, uy)
+    koch(d - 1, p1, s)
+    print("{0} {1}".format(s.x, s.y))
+    koch(d - 1, s, u)
+    print("{0} {1}".format(u.x, u.y))
+    koch(d - 1, u, t)
+    print("{0} {1}".format(t.x, t.y))
+    koch(d - 1, t, p2)
+
+
+n = int(input())
+p1: Point = Point(0.0, 0.0)
+p2: Point = Point(100.0, 0.0)
+print("{0} {1}".format(p1.x, p1.y))
+koch(n, p1, p2)
+print("{0} {1}".format(p2.x, p2.y))
