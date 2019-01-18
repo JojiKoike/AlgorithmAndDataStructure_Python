7f61aa5fd46a68a0b8d39d88656e636b1a5a211c
JojiKoike <joji.koike@gmail.com>
2019-01-18 12:56:06 +0900

    Compare Stability between Merge Sort and Quick Sort

--- chapter7/quick_sort.py
+++ chapter7/quick_sort.py
@@ -1,10 +1,45 @@
 from typing import List
 
+N_MAX: int = 100000
+SENTINEL: int = 10000000000
+
 
 class Card:
     def __init__(self, suit: str, value: int) -> None:
         self.suit: str = suit
         self.value: int = value
+
+
+L: List[Card] = [Card("", 0) for i in range(int(N_MAX / 2) + 2)]
+R: List[Card] = [Card("", 0) for j in range(int(N_MAX / 2) + 2)]
+
+
+def merge(a: List[Card], left: int, mid: int, right: int) -> None:
+    n1: int = mid - left
+    n2: int = right - mid
+    for i in range(n1):
+        L[i] = a[left + i]
+    L[n1].value = SENTINEL
+    for j in range(n2):
+        R[j] = a[mid + j]
+    k: int = 0
+    l: int = 0
+    L[n2].value = SENTINEL
+    for m in range(left, right):
+        if L[k].value <= R[l].value:
+            a[m] = L[k]
+            k += 1
+        else:
+            a[k] = R[l]
+            l += 1
+
+
+def merge_sort(a: List[Card], left: int, right: int) -> None:
+    if left + 1 < right:
+        mid: int = int((left + right) / 2)
+        merge_sort(a, left, mid)
+        merge_sort(a, mid, right)
+        merge(a, left, mid, right)
 
 
 def partition(cards: List[Card], p: int, r: int) -> int:
@@ -26,9 +61,21 @@ def quick_sort(cards: List[Card], p: int, r: int) -> None:
 
 # Get and build inputs
 n: int = int(input())
-cards: List[Card] = []
+cards_quick: List[Card] = []
+cards_merge: List[Card] = []
 for i in range(n):
     suit, value = input().split()
-    cards.append(Card(suit, int(value)))
-quick_sort(cards, 0, n - 1)
-print(cards)
+    cards_quick.append(Card(suit, int(value)))
+    cards_merge.append(Card(suit, int(value)))
+quick_sort(cards_quick, 0, n - 1)
+merge_sort(cards_merge, 0, n)
+
+stable: bool = True
+for i in range(n):
+    if cards_quick[i].suit != cards_merge[i].suit:
+        stable = False
+        break
+
+print(cards_quick)
+print(cards_merge)
+print("Stable" if stable else "Not Stable")
