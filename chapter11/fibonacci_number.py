2267de06a51237b6e3f1d943147411eeb7e868f2
JojiKoike <joji.koike@gmail.com>
2019-01-21 12:47:30 +0900

    Fibonacci Number Solved By DP

---
+++ chapter11/fibonacci_number.py
@@ -0,0 +1,18 @@
+from typing import List
+
+dp: List[int] = None
+
+
+def fibonacci(n: int) -> int:
+    if n == 0 or n == 1:
+        dp[n] = 1
+    else:
+        if dp[n] == -1:
+            dp[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return dp[n]
+
+
+n: int = int(input())
+dp = [-1 for i in range(n + 1)]
+print("Result: {0}".format(fibonacci(n)))
+print("DP Table:{0}".format(dp))
