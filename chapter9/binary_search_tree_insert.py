ebb52e58da1bf3e3c5baefa28ee4b12e27cf870e
JojiKoike <joji.koike@gmail.com>
2019-01-24 12:53:32 +0900

    Binary Search Tree (Insert)

---
+++ chapter9/binary_search_tree_insert.py
@@ -0,0 +1,66 @@
+from typing import List, Any
+
+class Node:
+    def __init__(self, key: int, left: Any, parent: Any, right: Any):
+        self.key = key
+        self.left = left
+        self.parent = parent
+        self.right = right
+
+root: Node
+NIL: Node
+
+def insert(k: int) -> None:
+    y: Node = NIL   # For Parent
+    x: Node = root  # For Next Node
+    z: Node = Node(k, NIL, NIL, NIL)
+
+    # Calc Parent Node
+    while x != NIL:
+        y = x
+        if z.key < y.key:
+            x = x.left
+        else:
+            x = x.right
+
+    z.parent = y
+    # Calc which side new node inserted into
+    if y == NIL:
+        root = z
+    else:
+        if z.key < y.left.key:
+            y.left = z
+        else:
+            y.right = z
+
+
+def inorder(u: Node) -> None:
+    if u == NIL:
+        return
+    inorder(u.left)
+    print(" {0}".format(u.key), end="")
+    inorder(u.right)
+
+
+def preorder(u: Node) -> Node:
+    if u == NIL:
+        return
+    print(" {0}".format(u.key), end="")
+    preorder(u.left)
+    preorder(u.right)
+
+
+n: int = int(input())
+for i in range(n):
+    inputs: List[str] = input().split(" ")
+    if len(inputs) == 2:
+        if inputs[0] == 'insert':
+            insert(int(inputs[1]))
+    elif inputs[0] == "print":
+        inorder(root)
+        print()
+        preorder(root)
+        print()
+
+
+
