05675df031811a42b453246c4b6ced46b01e376a
JojiKoike <joji.koike@gmail.com>
2019-01-25 12:57:41 +0900

    Binary Search Tree (Finished: insert, find, Under Coding: delete)

--- chapter9/binary_search_tree_delete.py
+++ chapter9/binary_search_tree_delete.py
@@ -1,6 +1,7 @@
 from typing import List, Any
 
-class Node:
+
+class Node(object):
     def __init__(self, key: int, left: Any, parent: Any, right: Any):
         self.key = key
         self.left = left
@@ -11,22 +12,19 @@ class Node:
 root: Any = None
 NIL: Any = None
 
+
 def insert(k: int) -> None:
     global root, NIL
-    y: Node = NIL   # For Parent
-    x: Node = root  # For Next Node
+    x: Node = root
+    y: Node = NIL
     z: Node = Node(k, NIL, NIL, NIL)
-
-    # Calc Parent Node
     while x != NIL:
         y = x
-        if z.key < y.key:
+        if z.key < x.key:
             x = x.left
         else:
             x = x.right
-
     z.parent = y
-    # Calc which side new node inserted into
     if y == NIL:
         root = z
     else:
@@ -34,6 +32,19 @@ def insert(k: int) -> None:
             y.left = z
         else:
             y.right = z
+
+
+def find(u: Node, k: int) -> Node:
+    while u != NIL and k != u.key:
+        if k < u.key:
+            u = u.left
+        else:
+            u = u.right
+    return u
+
+
+def delete(k: int) -> None:
+    return
 
 
 def inorder(u: Node) -> None:
@@ -44,21 +55,31 @@ def inorder(u: Node) -> None:
     inorder(u.right)
 
 
-def preorder(u: Node) -> Node:
+def preorder(u: Node) -> None:
     if u == NIL:
         return
     print(" {0}".format(u.key), end="")
     preorder(u.left)
     preorder(u.right)
 
+
+# Input, Solve
 n: int = int(input())
 for i in range(n):
-    inputs: List[str] = input().split(" ")
-    if len(inputs) == 2:
-        if inputs[0] == 'insert':
-            insert(int(inputs[1]))
-    elif inputs[0] == "print":
-        inorder(root)
-        print()
-        preorder(root)
-        print()
+    cmd: List[str] = input().split()
+    if len(cmd) == 2:
+        if cmd[0] == 'insert':
+            insert(int(cmd[1]))
+        elif cmd[0] == 'find':
+            res: Node = find(root, int(cmd[1]))
+            print("Yes" if res != NIL else "No")
+        elif cmd[0] == 'delete':
+            delete(int(cmd[0]))
+    else:
+        if cmd[0] == 'print':
+            inorder(root)
+            print()
+            preorder(root)
+            print()
+
+
