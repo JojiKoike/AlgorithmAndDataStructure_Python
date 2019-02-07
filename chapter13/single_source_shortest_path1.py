7a04ed821b0bc8d7fdfc260de60d4d41bcb53340
JojiKoike <joji.koike@gmail.com>
2019-02-07 23:23:50 +0900

    Single Source Shortest Path (Dijkstra Algorithm)

--- chapter13/single_source_shortest_path1.py
+++ chapter13/single_source_shortest_path1.py
@@ -1,20 +1,42 @@
+from enum import Enum
 from typing import List
 
 INFTY: int = 100000000
 
 
-def dijkstra(i: int) -> int:
-    return i
+class Color(Enum):
+    WHITE: int = 1
+    GRAY: int = 2
+    BLACK: int = 3
+
+
+def dijkstra() -> None:
+    d[0] = 0
+    color[0] = Color.GRAY
+    while True:
+        minv: int = INFTY
+        u: int = -1
+        for i in range(n):
+            if d[i] < minv and color[i] != Color.BLACK:
+                u = i
+                minv = d[i]
+        if u == -1: break
+        color[u] = Color.BLACK
+        for v in range(n):
+            if color[v] != Color.BLACK and m[u][v] != INFTY:
+                if d[v] > d[u] + m[u][v]:
+                    d[v] = d[u] + m[u][v]
+                    color[v] = Color.GRAY
+    for i in range(n):
+        print("{0} {1}".format(i, -1 if d[i] == INFTY else d[i]))
 
 
 n: int = int(input())
 m: List[List[int]] = [[INFTY for j in range(n)] for i in range(n)]
+d: List[int] = [INFTY for i in range(n)]
+color: List[Color] = [Color.WHITE for i in range(n)]
 for i in range(n):
     g: List[int] = list(map(int, input().split()))
     for j in range(1, g[1] + 1):
         m[g[0]][g[2 * j]] = g[2 * j + 1]
-
-print(m)
-
-for i in range(n):
-    print("{0} {1}".format(i, dijkstra(i)))
+dijkstra()
