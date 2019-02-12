8564cc904ea70e1b9865d8844f4bc05ec1aed66d
JojiKoike <joji.koike@gmail.com>
2019-02-12 12:59:47 +0900

    Add Index Explanation Comment

--- chapter11/matrix_chain_multiplication.py
+++ chapter11/matrix_chain_multiplication.py
@@ -13,6 +13,8 @@ for i in range(1, n + 1):
 for l in range(2, n + 1):
     for i in range(1, n - l + 2):
         j: int = i + l - 1
+        # i : Left Edge Matrix Index
+        # j : Right Edge Matrix Index
         for k in range(i, j):
             m[i][j] = min(m[i][j], m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j])
 
